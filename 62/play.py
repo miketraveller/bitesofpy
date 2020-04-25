84f562959dfd6cdd5aad568bb1b455c368d98ec4
miketraveller <mike.hambe@outlook.com>
2020-04-25 11:32:57 +1000

    sumission Bite 62 @ codechalleng.es

---
+++ 62/play.py
@@ -0,0 +1,26 @@
+#%%
+s1 = set([1,3,5])
+
+# %%
+2 in s1
+
+# %%
+3 in s1
+
+# %%
+print("test")
+
+# %%
+alist = list(range(1000000))
+aset = set(alist)
+
+# %%
+aset
+
+# %%
+3 in aset
+
+# %%
+'g' in aset
+
+# %%
